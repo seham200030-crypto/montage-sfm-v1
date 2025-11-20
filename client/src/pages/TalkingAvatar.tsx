1da0981e801b2d3054ffe54399e166d7e436cafc
seham200030-crypto <seham200030@gmail.com>
2025-11-20 04:40:02 +0300

    Create TalkingAvatar component for talking avatar UI

---
+++ client/src/pages/TalkingAvatar.tsx
@@ -0,0 +1,8 @@
+export default function TalkingAvatar() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🧑‍🚀 Talking Avatar</h1>
+      <p>واجهة إنتاج أفاتار يتكلم.</p>
+    </div>
+  );
+}
