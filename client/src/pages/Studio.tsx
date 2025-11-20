0afdeaa552c0135ae39e826b948e6e6a5ed257b1
seham200030-crypto <seham200030@gmail.com>
2025-11-20 04:37:39 +0300

    Add Studio component with initial content

---
+++ client/src/pages/Studio.tsx
@@ -0,0 +1,8 @@
+export default function Studio() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🎬 Smart Studio</h1>
+      <p>هنا راح نضيف نظام صناعة السيناريو + الشخصيات + الإنتاج.</p>
+    </div>
+  );
+}
